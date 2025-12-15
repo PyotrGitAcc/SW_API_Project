d609f381ee3ee0e77e376b638ed8b380cbaedc07
PyotrGitAcc <piter112006@mail.ru>
2025-12-15 19:45:15 +0300

    Adding build to the repo

---
+++ build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.Main"},{"l":"org.Main.HelperClasses"},{"l":"org.Main.UtilityClasses"}];updateSearchResults();
